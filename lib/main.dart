def1b9806aa57df60f8d00a6f1b10be3dbfa96f9
WongYewKeong <a175308@siswa.ukm.edu.my>
2021-12-26 16:03:56 +0800

    Add login register without design

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,28 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
-  
-  
-void main() => runApp(const MyApp());
-  
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'package:firebase_core/firebase_core.dart';
 
-  @override
-  State createState() => _State();
+import 'Ui/login.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MyApp(),
+  );
 }
-  
-class _State extends State<MyApp> {
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/",
-      routes: {
-        "/": (BuildContext context) =>  const HelloConvexAppBar(),
-      },
-    );
-  }
-}
-  
-class HelloConvexAppBar extends StatelessWidget {
-  const HelloConvexAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home'),
-        backgroundColor: Colors.blue),
-      body: const Center( ),
-      bottomNavigationBar: ConvexAppBar(
-        style: TabStyle.fixedCircle,
-        backgroundColor: Colors.blue,
-        items: [
-          TabItem(icon: Icons.home,title:'Home'),
-          TabItem(icon: Icons.add),
-          TabItem(icon: Icons.people,title:'Profilee'),
-        ],
-       
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home:LoginScreen(),
     );
   }
 }

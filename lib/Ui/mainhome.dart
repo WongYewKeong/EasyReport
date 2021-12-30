7678dd6d7e40ec7d8b9b5fa17fcc1cc500b1d0a9
WongYewKeong <a175308@siswa.ukm.edu.my>
2021-12-30 17:39:16 +0800

    Add AI chatbot

--- lib/Ui/mainhome.dart
+++ lib/Ui/mainhome.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:kommunicate_flutter/kommunicate_flutter.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Center(child: Text("Home Page",style: TextStyle(fontSize: 20),))
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async{
+          try {
+            dynamic conversationObject = {
+     'appId': '3e99e9ca11adde7870b721f2264ac8edc',// The [APP_ID](https://dashboard.kommunicate.io/settings/install) obtained from kommunicate dashboard.
+     };
+
+    dynamic result= await KommunicateFlutterPlugin.buildConversation(conversationObject);
+        
+      print("Conversation builder success : " + result.toString());
+    }on Exception catch (e) {
+      print("Conversation builder error : " + e.toString());
+    }
+        },
+          
+          
+        
+        backgroundColor: Colors.red[300],
+        tooltip: 'Chatbot',
+        label: Text("Chat"),
+        icon: const Icon(Icons.smart_toy_outlined,
+        
+      ),
       ),
     );
   }
